0cae9e9a777c514af5f8c12638a4fee140177886
rjaftab786 <aftab.thechamp786@gmail.com>
2022-04-30 20:04:57 +0530

    python

---
+++ main.py
@@ -0,0 +1,45 @@
+# name = "Aftab"
+# age = 32
+#
+# name = "Ahad"
+# age = 4
+#
+# print(name)
+# print(age)
+
+
+# Example 3:
+
+# first_name = "Aftab"
+# last_name = "Sheikh"
+# age = 32
+# is_adult = True
+#
+# print(first_name + " " + last_name)
+# print(age)
+# print(is_adult)
+
+
+# Taking Input
+# name = input("What is your name?")
+# print(" Hello " + name)
+# print("Welcome to cool python classes")
+
+# Type Conversation
+
+# old_age = input("Enter your age:")
+# # new age = old_age +2
+#
+# new_age = int(old_age) + 2
+# print(new_age)
+
+# Code for sum 2 numbers
+
+# first_number = input("Enter 1st number:")
+# second_number = input("Enter 2nd number:")
+#
+# sum = float(first_number) + float(second_number)
+# print("The sum is :" + str(sum))
+
+
+# String
